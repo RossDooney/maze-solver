9f44b4a9fc2f78f27605a81122293dc1419d1091
Ross Dooney <rossdooney@gmail.com>
2023-09-12 11:50:19 +0100

    Draw Cell added

--- graphics.py
+++ graphics.py
@@ -50,11 +50,12 @@ class Line:
 
 
 class Cell:
-    def __init__(self, x1, x2, y1, y2, win):
-        self.has_left_wall = False
-        self.has_right_wall = False
-        self.has_top_wall = False
-        self.has_bottom_wall = False
+    def __init__(self, walls, x1, y1, x2, y2, win):
+        #        self.has_left_wall = False
+        #        self.has_right_wall = False
+        #        self.has_top_wall = False
+        #        self.has_bottom_wall = False
+        self.has_wall = walls
         self._win = win
         # possible better to use a list for this? self.has_walls = [False, False, False, False]
         # top right
@@ -63,6 +64,20 @@ class Cell:
         # top left
         self._x2 = x2
         self._y2 = y2
+        print(self.has_wall)
+        if self.has_wall[0] == True:
+            top_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(top_wall, "black")
+        if self.has_wall[1] == True:
+            bottom_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(bottom_wall, "black")
+        if self.has_wall[2] == True:
+            right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(right_wall, "black")
+        if self.has_wall[3] == True:
+            left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(left_wall, "black")
 
-        l = Line(Point(self._x1, self._y1), Point(self._x2, self._y2))
-        self._win.draw_line(l, "black")
+
+#       l = Line(Point(self._x1, self._y1), Point(self._x2, self._y2))
+#       self._win.draw_line(l, "black")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     win = Window(800, 600)
     #    l = Line(Point(50, 50), Point(400, 400))
     #    win.draw_line(l, "black")
-    x = Cell(60, 70, 200, 750, win)
+    x = Cell([True, True, True, True], 50, 50, 200, 200, win)
     win.wait_for_close()
 
 
