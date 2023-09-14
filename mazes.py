95f08613a0ea521c344b4921cb9f374e4f98ab85
RossDooney <rossdooney@googlemail.com>
2023-09-14 15:46:23 +0100

    Maz random generation done

--- mazes.py
+++ mazes.py
@@ -5,7 +5,9 @@ import random
 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(
+        self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None
+    ):
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -16,6 +18,8 @@ class Maze:
         self._cells = []
         self._create_cells()
         self._break_walls_r(0, 0)
+        if seed:
+            random.seed(seed)
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -44,7 +48,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.03)
+        time.sleep(0.01)
 
     def _break_exit_and_enterance(self, i, j):
         self._cells[0][0].has_left_wall = False
@@ -62,17 +66,36 @@ class Maze:
             if i > 0 and not self._cells[i - 1][j].visited:
                 new_index_list.append((i - 1, j))
                 possible_directions += 1
-            if i < self._num_cols and not self._cells[i + 1][j].visited:
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
                 new_index_list.append((i + 1, j))
                 possible_directions += 1
             if j > 0 and not self._cells[i][j - 1].visited:
                 new_index_list.append((i, j - 1))
                 possible_directions += 1
-            if j < self._num_rows and not self._cells[i][j - 1].visited:
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
                 new_index_list.append((i, j + 1))
+                possible_directions += 1
 
             if possible_directions == 0:
                 self._draw_cell(i, j)
                 return
 
-            return
+            direction_index = random.randrange(possible_directions)
+            next_index = new_index_list[direction_index]
+
+            if next_index[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i + 1][j].has_left_wall = False
+            if next_index[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i - 1][j].has_right_wall = False
+
+            if next_index[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j + 1].has_top_wall = False
+
+            if next_index[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j - 1].has_bottom_wall = False
+
+            self._break_walls_r(next_index[0], next_index[1])
