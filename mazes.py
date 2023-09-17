3b709070ba7767c07e1fc8a8411ce857f1071fb7
RossDooney <rossdooney@googlemail.com>
2023-09-17 17:32:35 +0100

    solve finished

--- mazes.py
+++ mazes.py
@@ -112,14 +112,9 @@ class Maze:
     def _solve_r(self, i, j):
         self._animate()
 
-        # vist the current cell
         self._cells[i][j].visited = True
-
-        # if we are at the end cell, we are done!
         if i == self._num_cols - 1 and j == self._num_rows - 1:
             return True
-
-        # move left if there is no wall and it hasn't been visited
         if (
             i > 0
             and not self._cells[i][j].has_left_wall
@@ -131,7 +126,6 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i - 1][j], True)
 
-        # move right if there is no wall and it hasn't been visited
         if (
             i < self._num_cols - 1
             and not self._cells[i][j].has_right_wall
@@ -143,7 +137,6 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i + 1][j], True)
 
-        # move up if there is no wall and it hasn't been visited
         if (
             j > 0
             and not self._cells[i][j].has_top_wall
@@ -155,7 +148,6 @@ class Maze:
             else:
                 self._cells[i][j].draw_move(self._cells[i][j - 1], True)
 
-        # move down if there is no wall and it hasn't been visited
         if (
             j < self._num_rows - 1
             and not self._cells[i][j].has_bottom_wall
@@ -166,6 +158,4 @@ class Maze:
                 return True
             else:
                 self._cells[i][j].draw_move(self._cells[i][j + 1], True)
-
-        # we went the wrong way let the previous cell know by returning False
         return False
